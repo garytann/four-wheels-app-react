71bd16a76ff7c03691ee4c693a49b04f867db9a0
garytann <garytannjy@gmail.com>
2024-06-10 23:50:25 +0800

    added about us component with mobile responsiveness

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -6,14 +6,24 @@ function About() {
     <div className={styles.aboutContainer}>
         <div className={styles.itemContainer}>
             <div className={styles.headers}>
-                <h3>About Us</h3>
-                <h2>
-                    Why <span className="highlight">Learn</span> How to Skate With Us?
+                <p className={styles.aboutText}>About Us</p>
+                <h2 className={styles.title}>
+                    Individual & Group Coaching
                 </h2>
             </div>
             <p className={styles.text}>
-                We want our students to feel awesome and unique but at the same time, safe!
+                We offer individual and group coaching for all ages and levels. Our experienced coaches will help you to improve your skills and reach your goals.
+                We want our students to feel awesome and unique but at the same time, safe while learning!
             </p>
+
+            <div className={styles.buttons}>
+                <button className={styles.button}>
+                    Lessons & Programs
+                </button>
+                <button className={styles.button}>
+                    Our coaches
+                </button>
+            </div>
         </div>
         <img className={styles.image} src={about} alt="About Us"/>
     </div>
