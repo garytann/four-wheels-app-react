a9082b1ba70f8515d11f2aafa8460a75eeeb0efc
garytann <garytannjy@gmail.com>
2024-06-16 11:21:25 +0800

    added animation for quotes components:

--- my-app/src/components/Quote.js
+++ my-app/src/components/Quote.js
@@ -1,11 +1,20 @@
 import styles from "../Styles/quote.module.css"
+import {useEffect, useRef} from "react";
+import {animateOnScroll} from "../animation /Animation";
 
 function Quote (props){
+
+    const quoteContainerRef = useRef(null);
+    useEffect(() => {
+        console.log(quoteContainerRef.current);
+        // const elements = document.querySelectorAll(`.${styles.itemContainer}`);
+        animateOnScroll([quoteContainerRef.current], styles.animate);
+    }, []);
     return (
         <>
             <div className={styles.quoteContainer}>
                 <img className={styles.quoteImg} alt="quoteImg" src={props.quoteImg}/>
-                <div className={styles.textContainer}>
+                <div ref={quoteContainerRef} className={styles.textContainer}>
                     <p className={styles.quote}>{props.quoteText}</p>
                     <p className={styles.author}>{props.quoteAuthor}</p>
                 </div>
