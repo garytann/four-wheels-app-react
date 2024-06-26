270f5342dbbe80c1d932b77e44ac5bf59944121a
garytann <garytannjy@gmail.com>
2024-06-26 15:19:03 +0800

    added a material UI button to the hero component, i am not sure if its worth it

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -1,6 +1,65 @@
 import styles from "../Styles/hero.module.css";
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { keyframes } from '@mui/system';
+import {Link} from "react-router-dom";
+
 
 function Hero() {
+
+    const swipeUp = keyframes`
+          0% {
+            transform: translateY(100%);
+            opacity: 0;
+          }
+          100% {
+            transform: translateY(0);
+            opacity: 1;
+          }
+        `;
+    const isMobile = useMediaQuery('(max-width:768px)');
+
+    const BootstrapButton = styled(Button)({
+        animation: `${swipeUp} 1s ease-in-out`,
+        boxShadow: 'none',
+        textTransform: 'none',
+        color:'#EAEAEA',
+        fontSize: isMobile ? '1rem' : '2rem', // Reduce the font size on mobile
+        padding: isMobile ? '1rem 1rem' : '1rem 2rem', // Reduce the padding on mobile
+        border: '1px solid',
+        borderRadius:'35px',
+        lineHeight: 1.5,
+        backgroundColor: '#32373B',
+        borderColor: '#494b4b',
+        fontFamily: [
+            'Poppins',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:hover': {
+            backgroundColor: '#494b4b',
+            borderColor: '#494b4b',
+            boxShadow: 'none',
+        },
+        '&:active': {
+            boxShadow: 'none',
+            backgroundColor: '#B8B42D',
+            borderColor: '#B8B42D',
+        },
+        '&:focus': {
+            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+        },
+    });
+
   return (
       <div className={styles.heroContainer}>
           <div>
@@ -10,9 +69,12 @@ function Hero() {
                   <span>US</span>
               </div>
           </div>
-          <button className={styles.startButton}>
-              SKATES UP
-          </button>
+          <BootstrapButton variant="contained">
+              <Link to="/bookings">SKATES UP</Link>
+          </BootstrapButton>
+          {/*<button className={styles.startButton}>*/}
+          {/*    SKATES UP*/}
+          {/*</button>*/}
       </div>
   );
 }
