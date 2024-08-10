cdce5ebe6eac9d3d74555d705390dfb16712854a
garytann <garytannjy@gmail.com>
2024-08-10 15:26:29 +0800

    added lesson date in bookings model

--- backend/models/bookings.go
+++ backend/models/bookings.go
@@ -7,14 +7,15 @@ import (
 )
 
 type Bookings struct {
-	ID        uint `json:"id" gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-	Name      string         `json:"name"`
-	Contact   string         `json:"contact"`
-	Email     string         `json:"email"`
-	Pax       uint8          `json:"pax"`
-	Location  string         `json:"location"`
-	Timing    string         `json:"timing"`
+	ID         uint `json:"id" gorm:"primary_key"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	Name       string         `json:"name"`
+	Contact    string         `json:"contact"`
+	Email      string         `json:"email"`
+	Pax        uint8          `json:"pax"`
+	Location   string         `json:"location"`
+	Timing     string         `json:"timing"`
+	LessonDate string         `json:"lesson_date"`
 }
