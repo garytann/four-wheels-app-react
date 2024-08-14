0fd06d0d381eb74a693528f4eb642abb33164820
garytann <garytannjy@gmail.com>
2024-08-14 21:43:22 +0800

    added about page:

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -1,9 +1,26 @@
 import styles from '../Styles/aboutpage.module.css';
+import React from "react";
+import headCoachImg from "../assets/images/head_coach.jpg"
 
 function About (){
     return (
         <div className={styles.container}>
-            <h1>About page</h1>
+            <h1 className={styles.headers}>About Us</h1>
+            <div className={styles.titleContainer}>
+                <h2 className={styles.title}>Skating as a passion</h2>
+                <p className={styles.desc}>Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+            </div>
+
+            <div className={styles.coachContainer}>
+                <h2 className={styles.title}>Our Coaches</h2>
+                <div className={styles.textContainer}>
+                    <img src={headCoachImg} alt="headCoachImg"/>
+                    <h2 className={styles.text}>Head Coach</h2>
+                    <p className={styles.desc}>Lorem Ipsum is simply dummy text of the printing and typesetting
+                        industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                </div>
+            </div>
         </div>
     )
 }
